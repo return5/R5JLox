6f336ac9b343cbf72e834207a9fbf2e2057d4497
chris <armoredmoose@gmail.com>
2022-12-14 16:07:54 -0600

    updated tool

--- src/main/java/com/github/return5/tool/Tool.java
+++ src/main/java/com/github/return5/tool/Tool.java
@@ -15,14 +15,16 @@ public class Tool {
         }
         final String outputDirectory = args[0];
         defineAst(outputDirectory, "Expr",List.of("Binary<T> : final Expr left, final Token<T> operator, final Expr right",
-                "Grouping : final Expr expression","Literal<T> : final T value","Unary<T> : final Token<T> operator, final Expr right"));
+                "Grouping : final Expr expression","Literal<T> : final T value","Unary<T> : final Token<T> operator, final Expr right",
+                "Variable<T> : final Token<T> name"));
 
-        defineAst(outputDirectory,"Stmt",List.of("Expression : final Expr expression","Say : final Expr expression"));
+        defineAst(outputDirectory,"Stmt",List.of("Expression : final Expr expression",
+                "Say : final Expr expression","stash : final Token<T> name, final Expr initializer"));
 
     }
 
     private static void defineType(final PrintWriter writer,final String baseName, final String className, final String fieldList) {
-        writer.println("\tstatic class " + className + " extends " + baseName + " {");
+        writer.println("\tpublic static class " + className + " extends " + baseName + " {");
         final String stripClassName = className.split("<")[0].trim();
         writer.println();
         final String[] fields = fieldList.split(", ");
@@ -32,7 +34,7 @@ public class Tool {
         Arrays.stream(fields).map(s -> s.split("final [\\w<>]+ ")[1]).forEach(f -> writer.println("\t\t\tthis." + f + " = " + f + ";"));
         writer.println("\t\t}");
         writer.println();
-        writer.println("\t\t<R> R accept(final Visitor<R> visitor) {");
+        writer.println("\t\tpublic <R> R accept(final Visitor<R> visitor) {");
         writer.println("\t\t\treturn visitor.visit" + stripClassName + baseName + "(this);");
         writer.println("\t\t}");
         writer.println("\t}");
@@ -43,11 +45,11 @@ public class Tool {
         try (PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8)) {
             writer.println("package main.java.com.github.return5.jlox.;  //TODO fill in the correct package here");
             writer.println();
-            writer.println("abstract class " + baseName + "{");
+            writer.println("public abstract class " + baseName + "{");
             writer.println();
             defineVisitor(writer,baseName,types);
             writer.println();
-            writer.println("\tabstract<R> R accept(final Visitor<R> visitor);");
+            writer.println("\tpublic abstract<R> R accept(final Visitor<R> visitor);");
             writer.println();
             types.forEach(e -> defineType(writer,baseName,e.split(":")[0].trim(),e.split(":")[1].trim()));
             writer.println();
@@ -56,11 +58,11 @@ public class Tool {
     }
 
     private static void defineVisitor(final PrintWriter writer,final String baseName,final List<String> types) {
-        writer.println("\tinterface Visitor<R> {");
+        writer.println("\tpublic interface Visitor<R> {");
         types.stream()
                 .map(t -> t.split(":")[0].trim())
                 .map(t -> t.replace("<T>","<?>"))
-                .forEach(e -> writer.println("\t\tR visit" + e.split("<")[0].trim() + baseName + "(" + e + " " + baseName.toLowerCase() + ");"));
+                .forEach(e -> writer.println("\t\tpublic R visit" + e.split("<")[0].trim() + baseName + "(" + e + " " + baseName.toLowerCase() + ");"));
         writer.println("\t}");
     }
 }
